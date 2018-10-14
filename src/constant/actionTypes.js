7d7848a0c204183bad8c6a31f9ff8ea48c8b4f92
Ligar Mugi Syahid <mugisyahid@gmail.com>
2018-10-14 17:06:58 +0700

    hello world

--- src/constant/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const APP_LOAD = 'APP_LOAD';
-export const ASYNC_START = 'ASYNC_START';
-export const ASYNC_END = 'ASYNC_END';
-
-// auth
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
